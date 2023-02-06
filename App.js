c184730e15a505298856b98ce99944f0c98a5e74
Rajitkumar <krajit909@gmail.com>
2023-02-06 21:29:50 +0530

    stylesheet

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+const Name = (props)=>{
+  return(
+    <View>
+      <Text>
+      Name:  {props.name}
+      Age: {props.age}
+      </Text>
+      <Text style={styles.container}>
+      Name:  {props.name}
+      Age: {props.age}
+      </Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your</Text>
-      <StatusBar style="auto" />
+      {/* <Text>React-native</Text> */}
+      {/* <Name name="Rajit" age="23"/>
+      <Name name="Ankit" age="13"/> */}
+      {/* <Text 
+      style={{fontSize:28}}
+      onPress={()=>{
+        console.log("Text Clicked")
+      }}
+      selectable
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, qui eligendi obcaecati rem doloribus tempore iure necessitatibus, rerum laborum adipisci ad perferendis dolores voluptatum esse architecto modi autem voluptatem quae?
+      </Text> */}
+      <Text style={styles.bg}>
+        Style in React-native
+      </Text>
     </View>
   );
 }
@@ -15,6 +47,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
+  bg:{
+    backgroundColor: 'black',
+    color:'white'
+  }
 });
