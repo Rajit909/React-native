8bace90138b2648dad9311d6ca6a41f8a3f53de3
Rajitkumar <krajit909@gmail.com>
2023-02-15 23:08:47 +0530

    horizontal card

--- components/ElevatedCards.js
+++ components/ElevatedCards.js
@@ -1,17 +1,70 @@
-import {StyleSheet, View, Text } from 'react-native'
+import {StyleSheet, View, Text, ScrollView } from 'react-native'
 import React from 'react'
 
 
 export default function ElevatedCards() {
     return(
         <View>
-            <Text>
+            <Text style={styles.headingText}>
                 Elevated card
             </Text>
+            <ScrollView horizontal={true} style={styles.container}>
+                <View style={[styles.card, styles.cardElevated]}>
+                    <Text>
+                        Tap
+                    </Text>
+                </View>
+                <View style={[styles.card, styles.cardElevated]}>
+                    <Text>
+                        Tap
+                    </Text>
+                </View>
+                <View style={[styles.card, styles.cardElevated]}>
+                    <Text>
+                        Tap
+                    </Text>
+                </View>
+                <View style={[styles.card, styles.cardElevated]}>
+                    <Text>
+                        Tap
+                    </Text>
+                </View>
+                <View style={[styles.card, styles.cardElevated]}>
+                    <Text>
+                        Tap
+                    </Text>
+                </View>
+            </ScrollView>
         </View>
     )
 }
 
-const style = StyleSheet.create({
-
+const styles = StyleSheet.create({
+    headingText:{
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 10,
+      },
+      container:{
+        padding: 8
+      },
+      card: {
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 100,
+        height: 100,
+        borderRadius:4,
+        margin:8,
+        color: '#00000'
+      },
+      cardElevated: {
+        backgroundColor: '#CAD5E2',
+        elevation: 2,
+        shadowOffset: {
+            width:1,
+            height:1
+        },
+        shadowColor: '#333'
+      }
 })
