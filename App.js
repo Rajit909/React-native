b7c668009a50584a51068432535cf7ccfa643b8c
Rajitkumar <krajit909@gmail.com>
2023-02-17 22:18:09 +0530

    ActionCard created

--- App.js
+++ App.js
@@ -3,14 +3,18 @@ import React from 'react'
 import FlatCard from './components/FlatCard.js'
 import ElevatedCards from './components/ElevatedCards.js'
 import FancyCard from './components/FancyCard.js'
+import ActionCard from './components/ActionCard.js'
 
 const App = () => {
   return (
     <SafeAreaView>
         <ScrollView>
-        <FlatCard/>
-        <ElevatedCards/>
-        <FancyCard/>
+
+          <FlatCard/>
+          <ElevatedCards/>
+          <FancyCard/>
+          <ActionCard/>
+          
         </ScrollView>
     </SafeAreaView>
   )
---
+++ components/ActionCard.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View, Linking } from 'react-native'
+import React from 'react'
+
+export default function ActionCard() {
+
+  return (
+    <View>
+      <Text>ActionCard</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
